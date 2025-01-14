66a5a3ed687fa2334d7bc28f573a23a8b3988691
Fadarse <fadarse@gmail.com>
2025-01-14 20:18:32 +0100

    initial

---
+++ suchi.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+    alert("NONE RESPONSIVE! \nThis project is best viewed on laptops, computers or devices with width around 1024px, because it has not been designed to display responsively to other screen sizes. \n \nIt is a 'STATIC' project with HTML & CSS codes only and no bottons, inputs or other user experience features is expected to work. \n\nPLEASE ENJOY THE VIEW.")
+}
